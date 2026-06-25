9f32a7da45bd66f08a8b62dcb42f7410451685c0
Kevin P Tom <kevinptom4@gmail.com>
2026-06-25 13:01:01 +0530

    fix: make vite assets base path conditional for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace 'your-repo-name' with the exact name of your GitHub repository
-  base: '/cl-react-chatapp/', 
+  base: process.env.GITHUB_ACTIONS ? '/cl-react-chatapp/' : '/',
 })
